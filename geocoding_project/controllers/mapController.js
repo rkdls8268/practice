a70b3fdfa178e514babfe1283e72cbc6ba1b2d69
rkdls8268 <rkdls8268@gmail.com>
2020-08-17 17:26:03 +0900

    mapController 수정

--- geocoding_project/controllers/mapController.js
+++ geocoding_project/controllers/mapController.js
@@ -1,6 +1,13 @@
+const key = require('../config/key');
+
 const map = {
     getLatLong: async (req, res) => {
+        const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?
+            address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&
+            key=${key.api_key}`;
         
+        console.log('geocodeUrl: ', geocodeUrl);
+        return res.redirect(geocodeUrl);
     }
 }
 
