7256da15575c144ac4ae2af23d56eba9d23da717
rkdls8268 <rkdls8268@gmail.com>
2020-10-18 15:50:06 +0900

    add: practice.ts

---
+++ typescript_practice/src/practice.ts
@@ -0,0 +1,13 @@
+let count = 0;
+count += 1;
+// count = 'string'; // 에러 발생
+
+const message: string = 'hello typescript'; // : string 이런식으로 타입 지정
+
+const done: boolean = true;
+
+// 타입 지정 안해줘도되나?
+const numbers = [1, 2, 3];
+const msg1 = 'str';
+
+console.log(msg1);
